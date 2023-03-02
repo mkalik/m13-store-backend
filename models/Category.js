2d5e75e092f4762baebcad8a2d6d9d34403457e1
mkalik <mbadiankouyate@gmail.com>
2023-03-01 16:47:15 -0800

    created models as well as implemented tag routes

--- models/Category.js
+++ models/Category.js
@@ -5,16 +5,27 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+    {
+        // define columns
+        id: {
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
 );
 
 module.exports = Category;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,69 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+    // find all tags
+    // be sure to include its associated Product data
+    console.log('tag get route hit');
+    try {
+        const tags = await Tag.findAll();
+        res.send(tags);
+    } catch (err) {
+        res.send(err);
+    }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    console.log('tag get by id route hit');
+    try {
+        const id_tag = await Tag.findByPk(req.params.id);
+        res.send(id_tag);
+    } catch (err) {
+        res.send(err);
+    }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+    // create a new tag
+    console.log('tag post route hit');
+    try {
+        const newTag = await Tag.create({
+            tag_name: req.body.tag_name,
+        });
+        res.send(newTag);
+    } catch (err) {
+        res.json({
+            message: 'Couldnt make that tag for some reason',
+            error: err,
+        });
+    }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+    // update a tag's name by its `id` value
+    console.log('tag put route hit');
+    try {
+        const updateTag = await Tag.findByPk(req.params.id);
+        const updated = await updateTag.update({
+            tag_name: `${req.body.tag_name}`,
+        });
+        res.send(updated);
+    } catch (err) {
+        res.send({ message: 'couldnt update that tag', error: err });
+    }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+    // delete on tag by its `id` value
+    console.log('tag delete route hit');
+    try {
+        const deleteTag = await Tag.destroy({ where: { id: req.params.id } });
+        res.send({ message: 'tag deleted' });
+    } catch (err) {
+        res.send({ message: 'tag couldnt be deleted', error: err });
+    }
 });
 
 module.exports = router;
